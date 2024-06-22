d138fc7b865abcab8194bc33341b161e723b7c01
jordancox7483 <jcox7483@gmail.com>
2024-06-22 09:52:43 -0400

    update automate

--- Automate.bat
+++ Automate.bat
@@ -1,11 +1,15 @@
 @echo off
-REM Activate the virtual environment
-CALL "C:\Users\jcox\Documents\Lake Scrape Project\.venv\Scripts\activate.bat"
+REM Define the log file path
+set LOGFILE="C:\Users\jcox\Documents\Lake Scrape Project\batlog.txt"
 
-REM Run the Python script
-"C:\Users\jcox\Documents\Lake Scrape Project\.venv\Scripts\python.exe" "C:\Users\jcox\Documents\Lake Scrape Project\Lake_Level_Automate.py"
+REM Activate the virtual environment and redirect output to log file
+CALL "C:\Users\jcox\Documents\Lake Scrape Project\.venv\Scripts\activate.bat" >> %LOGFILE% 2>&1
 
-REM Run the Python script
-"C:\Users\jcox\Documents\Lake Scrape Project\.venv\Scripts\python.exe" "C:\Users\jcox\Documents\Lake Scrape Project\WriteToGithub.py"
+REM Run the first Python script and redirect output to log file
+"C:\Users\jcox\Documents\Lake Scrape Project\.venv\Scripts\python.exe" "C:\Users\jcox\Documents\Lake Scrape Project\Lake_Level_Automate.py" >> %LOGFILE% 2>&1
 
-pause
+REM Run the second Python script and redirect output to log file
+"C:\Users\jcox\Documents\Lake Scrape Project\.venv\Scripts\python.exe" "C:\Users\jcox\Documents\Lake Scrape Project\WriteToGithub.py" >> %LOGFILE% 2>&1
+
+REM Deactivate the virtual environment if needed
+REM deactivate.bat
