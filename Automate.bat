b68cf4ed29a20d354c7d787d20a2d4c2aa628144
jordancox7483 <jcox7483@gmail.com>
2024-06-22 09:46:50 -0400

    update automate

--- Automate.bat
+++ Automate.bat
@@ -4,3 +4,8 @@ CALL "C:\Users\jcox\Documents\Lake Scrape Project\.venv\Scripts\activate.bat"
 
 REM Run the Python script
 "C:\Users\jcox\Documents\Lake Scrape Project\.venv\Scripts\python.exe" "C:\Users\jcox\Documents\Lake Scrape Project\Lake_Level_Automate.py"
+
+REM Run the Python script
+"C:\Users\jcox\Documents\Lake Scrape Project\.venv\Scripts\python.exe" "C:\Users\jcox\Documents\Lake Scrape Project\WriteToGithub.py"
+
+pause
