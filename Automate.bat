5dc9db6d57c7f4ad4f078af7463d68d5d9ff8a5c
jordancox7483 <jcox7483@gmail.com>
2024-06-22 09:16:15 -0400

    Changes

---
+++ Automate.bat
@@ -0,0 +1,6 @@
+@echo off
+REM Activate the virtual environment
+CALL "C:\Users\jcox\Documents\Lake Scrape Project\.venv\Scripts\activate.bat"
+
+REM Run the Python script
+"C:\Users\jcox\Documents\Lake Scrape Project\.venv\Scripts\python.exe" "C:\Users\jcox\Documents\Lake Scrape Project\Lake_Level_Automate.py"
